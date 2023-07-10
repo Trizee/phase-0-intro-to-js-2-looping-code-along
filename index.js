845e6f0ae785f7173b004cd438eba0250eab3963
Tri <trihoang09@gmail.com>
2023-07-09 22:17:00 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Joe", "Jack", "John"]
+
+function writeCards(names){
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+    return array;
+}
+
+function countDown(){
+    let countDown = 10;
+    while (countDown > -1){
+        console.log(countDown --)
+    }
+}
